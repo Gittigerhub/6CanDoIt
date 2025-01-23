1b055cb0f1b6380dcb5e24fc44e3d697439ce50e
SIM KI HUN <tla6063801@gmail.com>
2025-01-23 16:27:14 +0900

    shopDetail 삽입,수정,삭제,자세히보기 입니다.

--- src/main/java/com/sixcandoit/roomservice/dto/office/ShopDetailDTO.java
+++ src/main/java/com/sixcandoit/roomservice/dto/office/ShopDetailDTO.java
@@ -1,42 +1,35 @@
 package com.sixcandoit.roomservice.dto.office;
 
+import com.sixcandoit.roomservice.entity.office.CalculateEntity;
+import com.sixcandoit.roomservice.entity.office.OrganizationEntity;
+import com.sixcandoit.roomservice.entity.orders.MenuEntity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class ShopDetailDTO {
-
-    private Integer idx;                        // 증감 키
-
-    private String shopDetailTel;               // 매장 연락처
-
-    private String shopDetailOpenTime;          // 매장 오픈 시간
-
-    private String shopDetailCloseTime;         // 매장 마감 시간
-
-    private String shopDetailRestDay;           // 매장 휴일
-
-    private int shopDetailOpenState;            // 매장 오픈 여부(0:영업 중, 1:종료)
-
-    private int shopDetailPartnerState;         // 매장 제휴 상태(0:제휴 중, 1:제휴 종료, 2:제휴 준비중)
-
-    private int shopDetailType;                 // 매장 타입(0:직영, 1:가맹)
-
-    private String bankNum;                     // 계좌 번호
-
-    private String bankName;                    // 계좌 은행명
-
-    private String bankOwner;                   // 계좌 소유주
-
-    private String activeYn;                    // 활성 유무(N:비 활성화, Y:활성화)
-
-    private float dayFee;                       // 일일 수수료
-
-    private float dayFeePercent;                // 일일 수수료 %
-
-    private OrganizationDTO organizationDTO;    // 조직 DTO
-
+    private Integer idx;                  // 기본 키
+    private String tel;                   // 매장 연락처
+    private LocalTime openTime;              // 매장 오픈시간
+    private LocalTime closeTime;             // 매장 마감시간
+    private String restDay;               // 매장 휴일
+    private int openState;                // 매장 상태(0:영업중, 1종료)
+    private int partnerState;             // 매장 제휴상태(0:제휴중, 1:제휴종료, 3:제휴준비중)
+    private int type;                     // 매장 타입(0:직영, 1:가맹)
+    private String bankNum;               // 매장 계좌번호
+    private String bankName;              // 매장 계좌 은행명
+    private String bankOwner;             // 매장 계좌 소유주
+    private String activeYn;              // 매장 활성화(N:비활성화, Y:활성화)
+    private float dayFee;                // 매장 일별 수수료
+    private float dayFeePercent;         // 매장 일별 수수료 %
 }
--- src/main/java/com/sixcandoit/roomservice/service/office/ShopDetailService.java
+++ src/main/java/com/sixcandoit/roomservice/service/office/ShopDetailService.java
@@ -1,12 +1,106 @@
 package com.sixcandoit.roomservice.service.office;
 
+import com.sixcandoit.roomservice.dto.office.ShopDetailDTO;
+import com.sixcandoit.roomservice.entity.office.ShopDetailEntity;
+import com.sixcandoit.roomservice.repository.office.ShopDetailRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ShopDetailService {
 
+    private  final ShopDetailRepository shopDetailRepository;
+    private  final ModelMapper modelMapper;
 
+    /*-------------------------------------------------
+    함수명 : register(ShopDetailDTO shopDetailDTO)
+    인수 : shopDetailDTO
+    출력 : 없음
+    설명 : 매장 정보를 등록할때 사용
+    ---------------------------------------------------*/
+    public void register(ShopDetailDTO shopDetailDTO){
+        try {
+            //Optional<ShopDetailEntity> read = shopDetailRepository.findById(shopDTO.getIdx());
+            ShopDetailEntity shopEntity = modelMapper.map(shopDetailDTO, ShopDetailEntity.class);
+            shopDetailRepository.save(shopEntity);
+        } catch (Exception e) {
+            throw new RuntimeException("상점 저장 실패 : "+e.getMessage());
+        }
+    }
 
+    /*-------------------------------------------------
+    함수명 : read(int idx)
+    인수 : 조회할 매장 idx
+    출력 : ShopDetailDTO
+    설명 : 매장을 조회할 때 사용
+  ---------------------------------------------------*/
+    public ShopDetailDTO read(Integer idx){
+        try {
+            Optional<ShopDetailEntity> read = shopDetailRepository.findById(idx);
+            if(!read.isPresent()){
+                throw new RuntimeException("상점 개별 조회 실패");
+            }
+            else {
+                ShopDetailDTO   shopDetailDTO = modelMapper.map(read,ShopDetailDTO.class);
+                return  shopDetailDTO;
+            }
+         } catch (Exception e){
+            throw new RuntimeException("상점 개별 조회 실패: "+e.getMessage());
+        }
+    }
+
+    /*-------------------------------------------------
+    함수명 : update(ShopDetailDTO shopDetail)
+    인수 : shopDetailDTO
+    출력 : 없음
+    설명 : 매장 정보를 수정할때 사용;
+  ---------------------------------------------------*/
+    public void update(ShopDetailDTO shopDetailDTO){
+        try {
+            Optional<ShopDetailEntity> read = shopDetailRepository.findById(shopDetailDTO.getIdx());
+            if(!read.isPresent()){
+                throw new RuntimeException("상점 조회 실패");
+            }
+            else {
+                ShopDetailEntity shopEntity =modelMapper.map(shopDetailDTO,ShopDetailEntity.class);
+                shopDetailRepository.save(shopEntity);
+            }
+        } catch (Exception e){
+            throw new RuntimeException("상점 수정 실패: "+e.getMessage());
+        }
+    }
+
+    /*-------------------------------------------------
+    함수명 : delete(ShopDetailDTO shopDetail)
+    인수 : shopDetailDTO
+    출력 : 없음
+    설명 : 매장 정보를 수정할때 사용
+    ---------------------------------------------------*/
+    public void delete(Integer idx){
+        try {
+            shopDetailRepository.deleteById(idx);
+        } catch (Exception e){
+            throw new RuntimeException("상점 삭제 실패: "+e.getMessage());
+        }
+    }
+
+    /*-------------------------------------------------
+    함수명 : list(Pageable page)
+    인수 : Pageable
+    출력 : 목록
+    설명 : 매장 정보들을 목록을 출력할때 사용
+    ---------------------------------------------------*/
+    public List<ShopDetailDTO> list(){
+        List<ShopDetailEntity> shoplist = shopDetailRepository.findAll();
+
+        List<ShopDetailDTO> shopDTOlist = Arrays.asList(modelMapper.map(shoplist, ShopDetailDTO[].class));
+
+        return shopDTOlist;
+    }
 }

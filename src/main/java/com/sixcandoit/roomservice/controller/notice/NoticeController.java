f39cc4c30c006201ce44186ea3dee54211b9adc1
Eunbi <eunbii0926@gmail.com>
2025-01-24 17:50:54 +0900

    notice

--- src/main/java/com/sixcandoit/roomservice/controller/notice/NoticeController.java
+++ src/main/java/com/sixcandoit/roomservice/controller/notice/NoticeController.java
@@ -3,25 +3,26 @@ package com.sixcandoit.roomservice.controller.notice;
 
 import com.sixcandoit.roomservice.dto.notice.NoticeDTO;
 import com.sixcandoit.roomservice.service.notice.NoticeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.swing.text.AbstractDocument;
 import java.util.List;
 
 @Controller // get, post 매핑만 사용이 가능
-//@RestController // create, modify, remove, get, put
 @RequiredArgsConstructor
-@Log // System.ot.println을 대체
+@Log
 
 public class NoticeController {
     private final NoticeService noticeService;
-
 
     //return중에서 중복되는 부분을 찾아서 맵핑이름과 동일한 부분이 기준
     // 전체 목록 페이지(기준)
@@ -58,32 +59,38 @@ public class NoticeController {
         NoticeDTO noticeDTO = noticeService.read(idx);  // 전달자가 있으면 변수로 받는다.
 
         log.info("개별정보를 페이지에 전달...");
-        model.addAttribute("noticeEntity", noticeDTO);
+        model.addAttribute("noticeDTO", noticeDTO);
 
         return "notice/read";
     }
     // 목록에서 수정버튼을 클릭했을 때 수정폼 페이지
-    @GetMapping("/update")
+    @GetMapping("/notice/update")
     public String updateForm(@RequestParam Integer idx, Model model){
         log.info("수정할 데이터 읽기...");
-        NoticeDTO noticeDTO = noticeService.read(idx);
+        NoticeDTO noticeDTO= noticeService.noticeRead(idx);
 
-        log.info("개별정보를 페이지에 전달...");
-        model.addAttribute("noticeEntity", noticeDTO);
+        log.info("개별 데이터를 페이지로 전달합니다.");
+        model.addAttribute("noticeDTO", noticeDTO);
 
         return "notice/update";
     }
     // 수정폼에서 수정버튼을 클릭했을 때 수정 처리
     @PostMapping("/notice/update")
-    public String updateProc(@ModelAttribute NoticeDTO noticeDTO){
+    public String updateProc(@Valid @ModelAttribute NoticeDTO noticeDTO, BindingResult bindingResult){
         log.info("수정된 데이터 저장...");
-        noticeService.update(noticeDTO);
+
+        if (bindingResult.hasErrors()) {
+            log.info("유효성 검사 오류 발생");
+            return "notice/update";
+        }
+        //유효성 검사 성공 시 수정 처리
+        noticeService.noticeUpdate(noticeDTO);
 
         return "redirect:/notice/list";
     }
     // 목록에서 삭제버튼을 클릭했을 때 삭제 처리
     //@GetMapping("/delete/{id}") = rest방식
-    @GetMapping("/delete")
+    @GetMapping("/notice/delete")
     public String deleteProc(@RequestParam Integer idx){
         log.info("삭제 처리...");
         noticeService.delete(idx);

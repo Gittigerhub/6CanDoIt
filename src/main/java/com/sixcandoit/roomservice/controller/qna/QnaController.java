6a25916da66632b106d205d0866379ee65acd4d2
world <08sungyu@gmail.com>
2025-02-03 17:34:42 +0900

    QnA 답변 등록, 삭제 구현

--- src/main/java/com/sixcandoit/roomservice/controller/qna/QnaController.java
+++ src/main/java/com/sixcandoit/roomservice/controller/qna/QnaController.java
@@ -12,12 +12,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
--- src/main/java/com/sixcandoit/roomservice/controller/qna/ReplyController.java
+++ src/main/java/com/sixcandoit/roomservice/controller/qna/ReplyController.java
@@ -2,6 +2,7 @@ package com.sixcandoit.roomservice.controller.qna;
 
 import com.sixcandoit.roomservice.dto.qna.QnaDTO;
 import com.sixcandoit.roomservice.dto.qna.ReplyDTO;
+import com.sixcandoit.roomservice.entity.qna.ReplyEntity;
 import com.sixcandoit.roomservice.service.qna.QnaService;
 import com.sixcandoit.roomservice.service.qna.ReplyService;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/reply") //url 프리픽스를 /reply로 고정
+import java.security.Principal;
+import java.util.Optional;
+
 @Controller
 @RequiredArgsConstructor
 @Log
@@ -20,9 +23,9 @@ public class ReplyController {
     private final ReplyService replyService;
     private final QnaService qnaService;
 
-    // Qna의 A 읽기
-    // Qna의 A 등록
-    @PostMapping("/register/{idx}")
+    // Qna의 A 읽기 -> Q 상세보기에서 목록 출력
+    // Qna의 A 등록 -> 모달 처리
+    @PostMapping("/reply/register")
     public String register(@RequestParam Integer qnaIdx,
                            @ModelAttribute ReplyDTO replyDTO,
                            BindingResult bindingResult,
@@ -37,9 +40,39 @@ public class ReplyController {
 
         return "redirect:/qna/read?idx=" + qnaIdx;
     }
+
     // Qna의 A 수정
+    @PostMapping("/reply/update")
+    public String update(@PathVariable("idx") Integer idx, Integer qnaIdx) {
+
+        ReplyEntity replyEntity = this.replyService.getReply(idx);
+
+        replyEntity.setReplyTitle(replyEntity.getReplyTitle());
+        replyEntity.setReplyContents(replyEntity.getReplyContents());
+
+        // 수정 후 해당 Q&A 상세 페이지로 리다이렉트
+        return "redirect:/qna/read?idx=" + qnaIdx;
+
+    }
 
     // Qna의 A 삭제
+    @GetMapping("/reply/delete")
+    public String delete(@RequestParam Integer idx, Integer qnaIdx) {
+        log.info("idx가 null인지 확인 중...");
+        if (idx == null) {
+            // idx가 null인 경우 에러 처리
+            return "redirect:/error";
+        }
 
+        log.info("데이터를 삭제합니다.");
+        // 답변 삭제
+        replyService.replyDelete(idx);
 
+        // 삭제 후 해당 질문 페이지로 리다이렉트
+        log.info("질문 페이지로 리다이렉트하는 중...");
+        if (qnaIdx == null) {
+            return "redirect:/error";
+        }
+        return "redirect:/qna/read?idx=" + qnaIdx;
+    }
 }

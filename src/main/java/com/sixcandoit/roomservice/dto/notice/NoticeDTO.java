7dd16a8a995386c908f5c82f3d83391c0bbd6288
EB <eunbii0926@gmail.com>
2025-02-07 18:10:26 +0900

    notice

--- src/main/java/com/sixcandoit/roomservice/dto/notice/NoticeDTO.java
+++ src/main/java/com/sixcandoit/roomservice/dto/notice/NoticeDTO.java
@@ -7,8 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+
 
 @Getter
 @Setter
@@ -18,17 +20,20 @@ import java.time.LocalDateTime;
 public class NoticeDTO {
 
     private Integer idx; //공지사항 키
-    private String  noticeTitle; //제목
+
+    private String noticeTitle; //제목
 
     @NotBlank(message = "내용을 입력하세요.")
     @Size(min = 1, message = "내용은 공백일 수 없습니다.")
 
-    private String  noticeContents; //내용
+    private String noticeContents; //내용
+
     private String noticeType; //공지타입
+
     private int noticeHits; //조회수
-    private LocalDateTime noticeDate; //작성일
 
-
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate noticeDate;//작성일
 
 
 }

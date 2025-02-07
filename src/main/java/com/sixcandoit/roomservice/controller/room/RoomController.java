3b6858bd0e624502ee55e5c1fba0ec26c7b825b6
world <08sungyu@gmail.com>
2025-02-07 17:58:35 +0900

    룸 등록 및 조회 구현

--- src/main/java/com/sixcandoit/roomservice/controller/room/RoomController.java
+++ src/main/java/com/sixcandoit/roomservice/controller/room/RoomController.java
@@ -10,6 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +21,16 @@ public class RoomController {
 
     // final 선언
     private final RoomService roomService;
+
+    // 룸 전체 목록
+    @GetMapping("/room/list")
+    private String list(Model model){
+        log.info("모든 객실 정보를 읽어온다...");
+        List<RoomDTO> roomDTOList = roomService.roomList();
+
+        model.addAttribute("roomDTOList", roomDTOList);
+        return "room/list";
+    }
 
     // 룸 등록
     @GetMapping("/room/register")
@@ -33,14 +46,41 @@ public class RoomController {
     @PostMapping("/room/register")
     public String registerProc(@Valid @ModelAttribute RoomDTO roomDTO){
         log.info("새로운 룸을 등록합니다.");
-
+        // roomWifi, roomTv, roomAir, roomBath가 null일 경우 N으로 설정
+        if (roomDTO.getRoomWifi() == null) {
+            roomDTO.setRoomWifi("N");
+        }
+        if (roomDTO.getRoomTv() == null) {
+            roomDTO.setRoomTv("N");
+        }
+        if (roomDTO.getRoomAir() == null) {
+            roomDTO.setRoomAir("N");
+        }
+        if (roomDTO.getRoomBath() == null) {
+            roomDTO.setRoomBath("N");
+        }
+        if (roomDTO.getRoomBreakfast() == null) {
+            roomDTO.setRoomBreakfast("N");
+        }
+        if (roomDTO.getRoomSmokingYn() == null) {
+            roomDTO.setRoomSmokingYn("N");
+        }
         // 등록 처리
         roomService.roomRegister(roomDTO);
 
-        return "redirect:/room/register";
+        return "redirect:/room/list";
     }
     // 룸 수정
     // 룸 삭제
-    // 룸 목록
     // 룸 상세보기
+    @GetMapping("/qna/detail")
+    public String detail(@RequestParam Integer idx, Model model){
+        log.info("개별 데이터를 읽는 중입니다.");
+        RoomDTO roomDTO = roomService.roomDetail(idx);
+
+        log.info("개별 데이터를 페이지에 전달하는 중입니다.");
+        model.addAttribute("roomDTO", roomDTO);
+
+        return "room/detail";
+    }
 }
--- src/main/java/com/sixcandoit/roomservice/dto/room/RoomDTO.java
+++ src/main/java/com/sixcandoit/roomservice/dto/room/RoomDTO.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Getter
 @Setter
@@ -16,12 +17,23 @@ public class RoomDTO {
     private String roomName;                // 객실 이름
     private String roomInfo;                // 객실 정보
     private String roomType;                // 객실 타입(STD:스탠다드, ROH:런 오브 하우스, MOD:모더레이트, SUP:슈페리어, DLX:디럭스, STU:스튜디오, JRSTE:쥬니어 스위트, STE:스위트, PH:팬트하우스)
-    private int roomPrice;                  // 객실 가격
+    private String roomView;                // 객실 뷰 종류(O:오션뷰, C:시티뷰, M:마운틴뷰, G:가든뷰)
+    private int roomNum = 2;                // 투숙객 수
+    private int roomBed = 0;                // 침대 타입(3: 킹, 2: 퀸, 1: 더블, 0: 싱글)
+    private int roomPrice = 0;                  // 객실 가격
+    private int roomSize = 0;                 // 객실 평수
     private String roomSeason;              // 시즌 (peak:성수기, off:비 성수기)
+    private String roomBreakfast = "N";              // 조식 여부 (Y: 있음, N: 없음)
     private String roomCondition;           // 객실 청소 여부( N:청소 미 완료, Y:청소 완료)
-    private int roomReservation;            // 객실 예약 상태(1:빈 방, 2:예약, 3:체크인, 4:체크 아웃)
-    private String roomSmokingYn;           // 객실 흡연 가능 여부(N:불가능, Y:가능)
-    private LocalDateTime roomCheckIn;      // 체크인 가능 시간
-    private LocalDateTime roomCheckOut;     // 체크 아웃 시간
-    private float roomCancelPee;
+    private String roomSmokingYn = "N";           // 객실 흡연 가능 여부(N:불가능, Y:가능)
+    private String roomWifi = "N";              // 와이파이 여부(N:없음, Y:있음)
+    private String roomTv = "N";                // TV 여부(N:없음, Y:있음)
+    private String roomAir = "N";               // 에어컨 여부(N:없음, Y:있음)
+    private String roomBath = "N";              // 전용욕실 여부(N:없음, Y:있음)
+    private LocalTime roomCheckIn;      // 체크인 가능 시간
+    private LocalTime roomCheckOut;     // 체크 아웃 시간
+    private float roomCancelPee;            // 취소 수수료
+    private int roomReservation  = 0;            // 객실 예약 상태(0:빈 방, 1:예약, 2:체크인, 3:체크 아웃)
+    private LocalDateTime insDate;      // 작성 일자
+    private LocalDateTime modDate;      // 수정 일자
 }

90247f7f5bbe67fb3a41e64bc0710f8e49392da9
EB <eunbii0926@gmail.com>
2025-02-14 17:54:38 +0900

    notice-

--- src/main/java/com/sixcandoit/roomservice/service/notice/NoticeService.java
+++ src/main/java/com/sixcandoit/roomservice/service/notice/NoticeService.java
@@ -3,17 +3,20 @@ package com.sixcandoit.roomservice.service.notice;
 import com.sixcandoit.roomservice.dto.notice.NoticeDTO;
 import com.sixcandoit.roomservice.entity.notice.NoticeEntity;
 import com.sixcandoit.roomservice.repository.notice.NoticeRepository;
+import com.sixcandoit.roomservice.service.S3Uploader;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
@@ -21,23 +24,47 @@ import java.util.Optional;
 @Log
 @RequiredArgsConstructor // Lombok을 이용한 생성자 자동 생성
 public class NoticeService {
+    @Value("images")
+    private String imgUploadLocation;
 
     private final NoticeRepository noticeRepository; // final로 설정 (반드시 주입되어야 함)
     private final ModelMapper modelMapper;
-    public void noticeRegister(NoticeDTO noticeDTO) {
+    private final S3Uploader s3Uploader;
+
+    public void noticeRegister(NoticeDTO noticeDTO, MultipartFile  file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        String newFileName ="";
+
+        if(originalFilename !=null){
+            newFileName = s3Uploader.upload(file,imgUploadLocation);
+        }
+        noticeDTO.setNoticeImg(newFileName);
+
+
         NoticeEntity noticeEntity =
                 modelMapper.map(noticeDTO, NoticeEntity.class);
         //저장
         noticeRepository.save(noticeEntity);
     }
 
-    public void noticeUpdate(NoticeDTO noticeDTO) {
+    public void noticeUpdate(NoticeDTO noticeDTO,MultipartFile  file) throws IOException {
+
         //데이터의 idx를 조회
         Optional<NoticeEntity> noticeEntity = noticeRepository.findById(noticeDTO.getIdx());
+        String deleteFile = noticeDTO.getNoticeImg();
+        String originalFilename = file.getOriginalFilename();
+        String newFileName ="";
 
         if (noticeEntity.isPresent()) {
             NoticeEntity noticeEntity1 = modelMapper.map(noticeDTO, NoticeEntity.class);
             noticeRepository.save(noticeEntity1);
+        }
+        if(originalFilename.length()!=0){
+            if(deleteFile.length()!=-0){
+                s3Uploader.deleteFile(deleteFile,imgUploadLocation);
+            }
+            newFileName = s3Uploader.upload(file,imgUploadLocation);
+            noticeDTO.setNoticeTitle(newFileName);
         }
     }
 

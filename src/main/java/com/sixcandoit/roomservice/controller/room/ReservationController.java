af75fb459c8db177041fde34b12b511591c6908b
world <08sungyu@gmail.com>
2025-03-04 12:50:27 +0900

    룸 예약 상태 변경(체크인, 체크아웃 등) 기능 구현

--- src/main/java/com/sixcandoit/roomservice/controller/room/ReservationController.java
+++ src/main/java/com/sixcandoit/roomservice/controller/room/ReservationController.java
@@ -43,7 +43,7 @@ public class ReservationController {
         // 조회된 룸 데이터와 멤버 데이터를 모델에 추가
         model.addAttribute("roomDTOList", roomDTOList);
 
-        return "reserve/insert"; // pageInsert.html 뷰 템플릿을 찾아서 렌더링합니다.
+        return "reserve/insert"; // Insert.html 뷰 템플릿을 찾아서 렌더링
     }
 
     // 예외 발생 시 에러 메시지 전달
@@ -85,7 +85,7 @@ public class ReservationController {
         // 모델에 빈 객실 목록을 추가
         model.addAttribute("Rooms", availableRooms);
 
-        return "/reserve/list"; // 빈 객실 목록을 보여주는 뷰 템플릿을 렌더링합니다.
+        return "/reserve/list"; // 빈 객실 목록을 보여주는 뷰 템플릿을 렌더링
     }
 
     //목록페이지로 이동
@@ -105,29 +105,7 @@ public class ReservationController {
         model.addAttribute("sdate", sdate);
         model.addAttribute("edate", edate);
 
-        return "reserve/list"; // page-list.html 뷰 템플릿을 찾아서 렌더링합니다.
-    }
-
-    //관리자용 예약 목록 리스트
-    @GetMapping("/reserve")
-    public String reslist(@RequestParam(name = "sdate", required = false)@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate sdate,
-                              @RequestParam(name = "edate", required = false)@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate edate,
-                              Model model) {
-        log.info("데이터 조회 후 목록페이지로 이동....");
-
-        // 리다이렉트 후 전달된 성공 또는 실패 메시지를 받아옵니다.
-        if (model.containsAttribute("errorMessage")) {
-            // 에러 메시지가 있으면 모달을 띄워주도록 합니다.
-            model.addAttribute("errorMessage", model.getAttribute("errorMessage"));
-        }
-
-        List<ReservationDTO> reserveDTOList = reservationService.reserveList(sdate, edate);
-
-        model.addAttribute("List", reserveDTOList);
-        model.addAttribute("sdate", sdate);
-        model.addAttribute("edate", edate);
-
-        return "room/reserve";
+        return "reserve/list"; // page-list.html 뷰 템플릿을 찾아서 렌더링
     }
 
     @GetMapping("/detail")
@@ -136,7 +114,7 @@ public class ReservationController {
         ReservationDTO reserveDTO = reservationService.reserveRead(idx);
 
         model.addAttribute("data", reserveDTO);
-        return "reserve/detail"; // page-details.html 뷰 템플릿을 찾아서 렌더링합니다.
+        return "reserve/detail"; // page-details.html 뷰 템플릿을 찾아서 렌더링
     }
 
     @GetMapping("/update/{idx}")
@@ -152,7 +130,7 @@ public class ReservationController {
 
         model.addAttribute("data", reserveDTO);
         model.addAttribute("roomDTOList", roomDTOList);
-        return "reserve/update"; // update-form.html 뷰 템플릿을 찾아서 렌더링합니다.
+        return "reserve/update"; // update-form.html 뷰 템플릿을 찾아서 렌더링
     }
 
     // 예약 수정 처리

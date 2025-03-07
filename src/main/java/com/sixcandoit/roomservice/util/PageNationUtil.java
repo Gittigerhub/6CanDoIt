a27f110f54a09b2d0a77e1c952e1af2dbc056d82
world <08sungyu@gmail.com>
2025-03-07 12:33:30 +0900

    룸에 조직 값 idx 받아오기 구현

--- src/main/java/com/sixcandoit/roomservice/util/PageNationUtil.java
+++ src/main/java/com/sixcandoit/roomservice/util/PageNationUtil.java
@@ -12,7 +12,7 @@ public class PageNationUtil {
 
     public static Map<String, Integer> Pagination(Page<?> page) {
         Map<String, Integer> map = new HashMap<>();            // 결과를 저장할 변수
-
+        
         int currentPage = page.getNumber()+1;                  // 현제페이지 번호
         int blockLimit = 5;                                    // 화면에 출력할 페이지번호의 수(1,2,3,4,...,10)
 
@@ -22,20 +22,19 @@ public class PageNationUtil {
 
         // 계산된 끝페이지 번호가 전체페이지 번호보다 크면 전체페이지
         int endPage = page.getTotalPages();                     // 마지막 페이지 번호
-
+        
         // 이전/다음 페이지 번호
         int prevPage = Math.max(1, currentPage-1);              // 계산된 페이지가 1보다 작으면 1페이지를 적용
         int nextPage = Math.min(endPage, currentPage+1);
-
+        
         // 처음(1) 이전(prevPage) 1(startPage) 2 3(currentPage) 4 5(endPage) 다음(nextPage) 마지막(lastPage)
         map.put("prevPage", prevPage);
         map.put("startPage", startPage);
         map.put("currentPage", currentPage);
         map.put("endPage", endPage);
         map.put("nextPage", nextPage);
-
+        
         return map;
 
     }
-
 }

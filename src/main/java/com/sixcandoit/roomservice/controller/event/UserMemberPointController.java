756fc8067a172738cb03db1fc9fc541cc5869c9d
SKH <tla6063801@gmail.com>
2025-03-07 17:25:26 +0900

    맵버 포인트,유저 포인트(페이지 네이션 수정)

--- src/main/java/com/sixcandoit/roomservice/controller/event/UserMemberPointController.java
+++ src/main/java/com/sixcandoit/roomservice/controller/event/UserMemberPointController.java
@@ -11,6 +11,7 @@ import com.sixcandoit.roomservice.service.member.MemberService;
 import com.sixcandoit.roomservice.util.PageNationUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -19,6 +20,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,32 +39,39 @@ public class UserMemberPointController {
     //유저 맴버 리스트
     @GetMapping("/usermemberpoint")
     public String UserPointGet(@PageableDefault(page = 1) Pageable page,
-                               @RequestParam(value = "type", defaultValue = "")String type,
-                               @RequestParam(value = "keyword", defaultValue = "")String keyword
-                                ,Model model, Principal principal) {
+                               @RequestParam(value = "type", defaultValue = "") String type,
+                               @RequestParam(value = "keyword", defaultValue = "") String keyword,
+                               @RequestParam(value = "start", defaultValue = "") LocalDateTime start,
+                               @RequestParam(value = "end", defaultValue = "") LocalDateTime end,
+                               Model model, Principal principal) {
 
         Optional<MemberEntity> memberEntity = memberRepository.findByMemberEmail(principal.getName());
 
         if (memberEntity.isEmpty()) {
             throw new RuntimeException("회원이 없습니다.");
         } else {
-            Page<MemberPointDTO> memberPointDTO = userMemberPointService.memberlist(memberEntity.get().getIdx(),page,type,keyword);
-            MemberPointDTO memberName = userMemberPointService.read(memberEntity.get().getIdx());
+
+            Page<MemberPointDTO> memberPointDTO = userMemberPointService.memberlist(memberEntity.get().getIdx(), page, type, keyword, start, end);
             Map<String, Integer> pageInfo = PageNationUtil.Pagination(memberPointDTO);
             model.addAttribute("memberPointDTO", memberPointDTO);
-            model.addAttribute("memberName", memberName);
             model.addAllAttributes(pageInfo);
             model.addAttribute("type", type);
             model.addAttribute("keyword", keyword);
+            model.addAttribute("start", start);
+            model.addAttribute("end", end);
+
+
         }
 
 
-        return "/event/usermemberpoint";
+        return "event/usermemberpoint";
     }
+
+
     //유저 맴버 읽기
     @GetMapping("/usermemberpoint/read")
     @ResponseBody
-    public Map<String, Object> UserPointRead(@RequestParam Integer idx, Map map) {
+    public Map<String, Object> UserPointRead(@RequestParam Integer idx) {
         MemberPointDTO userPoint = userMemberPointService.read(idx);
         Map<String, Object> response = new HashMap<>();
         response.put("userPoint", userPoint);
